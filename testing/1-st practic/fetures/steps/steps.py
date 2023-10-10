1a7d1e4e721129ffc3fa6b9beee37e7a228ed7e4
7invincible7 <113597597+sambukalx@users.noreply.github.com>
2023-10-11 01:36:57 +0300

    Create steps.py

---
+++ testing/1-st practic/fetures/steps/steps.py
@@ -0,0 +1,57 @@
+from behave import *
+from twentyone import *
+
+
+@given('a dealer')
+def step_impl(context):
+    context.dealer = Dealer()
+
+
+## NEW STEP
+@given('a hand {total:d}')
+def step_impl(context, total):
+    context.dealer = Dealer()
+    context.total = total
+
+
+@given('a {hand}')
+def step_impl(context, hand):
+    context.dealer = Dealer()
+    context.dealer.hand = hand.split(',')
+
+
+@when('the dealer sums the cards')
+def step_impl(context):
+    context.dealer_total = context.dealer.get_hand_total()
+
+
+@when('the round starts')
+def step_impl(context):
+    context.dealer.new_round()
+
+
+@when('the dealer determines a play')
+def step_impl(context):
+    context.dealer_play = context.dealer.determine_play(context.total)
+
+
+@then('the dealer gives itself two cards')
+def step_impl(context):
+    assert (len(context.dealer.hand) == 2)
+
+
+# NEW STEP
+@then('the dealer chooses a play')
+def step_impl(context):
+    assert (context.dealer.make_play() in ['stand', 'hit'])
+
+
+@then('the {total:d} is correct')
+def step_impl(context, total):
+    assert (context.dealer_total == total)
+
+
+## NEW STEP
+@then('the {play} is correct')
+def step_impl(context, play):
+    assert (context.dealer_play == play)
